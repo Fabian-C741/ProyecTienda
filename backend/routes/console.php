db85dc4f1fc7442fe7f7d827fad3cf5baa6dd71f
Fabian-C741 <electrofabi.serviciostecnicos@gmail.com>
2025-12-07 02:12:36 -0300

    Add deployment scripts and missing routes

---
+++ backend/routes/console.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Schedule;
+
+/*
+|--------------------------------------------------------------------------
+| Console Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of your console based routes
+| using closures. This provides a simple way to define console routes
+| without having to create command classes.
+|
+*/
